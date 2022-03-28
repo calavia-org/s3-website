d436b2d836fef17a253ae60eeb19b108748ffd5f
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2022-03-28 23:06:02 +0200

    New: Move s3-bucket creation to local module

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ module "s3_root_bucket" {
     Site      = var.site_domain
   }
 }
+
+module "s3_www_bucket" {
+  source = "./modules/s3-bucket"
+
+  bucket_acl  = "private"
+  bucket_name = "www.${var.site_domain}"
+  kms_key_id  = aws_kms_key.app_key.arn
+
+  tags = {
+    Terraform = "true"
+    Site      = var.site_domain
+  }
+}
