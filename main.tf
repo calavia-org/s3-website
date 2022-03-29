257b9028cf129227edf3c0b7e0e831dbb8c9d8f1
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2022-03-29 17:06:00 +0200

    New: Move website config to local module

--- main.tf
+++ main.tf
@@ -42,3 +42,26 @@ module "s3_www_bucket" {
     Site      = var.site_domain
   }
 }
+
+module "s3_root_website_config" {
+  source    = "./modules/s3-website"
+  bucket_id = module.s3_root_bucket.id
+
+  redirect_all_request_to_target = {
+    host_name = "www.${var.site_domain}"
+    protocol  = "https"
+  }
+}
+
+
+# default = [
+#   {
+#     internal = 8300
+#     external = 8300
+#     protocol = "tcp"
+#   }
+# ]
+# allowed_headers = ["Authorization", "Content-Length"]
+# allowed_methods = ["GET"]
+# allowed_origins = ["https://www.${var.domain_name}"]
+# max_age_seconds = 3000
